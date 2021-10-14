af146ca5ee180f12df35ee48ff55f2d57246ea30
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2021-10-14 10:18:01 -0400

    feat: explicação sobre abertura e fechamento de transação.

--- src/test/java/com/algaworks/ecommerce/iniciandocomjpa/ConsultandoRegistrosTest.java
+++ src/test/java/com/algaworks/ecommerce/iniciandocomjpa/ConsultandoRegistrosTest.java
@@ -10,14 +10,7 @@ public class ConsultandoRegistrosTest extends EntityManagerTest {
 	@Test
 	public void buscarPorIdentificador() {
 		Produto produto = entityManager.find(Produto.class, 1);
-		
-		//get reference vai fazer uma busca no banco só se for utilizado algum atributo de Produto, como por exemplo produto.getNome().		
-//		Produto produto = entityManager.getReference(Produto.class, 1);
-
-		//o retorno não pode ser null		
 		Assert.assertNotNull(produto); 
-		
-		//o retorno de produto.nome tem que ser Kindle		
 		Assert.assertEquals("Kindle", produto.getNome()); 
 	}
 	
@@ -25,12 +18,10 @@ public class ConsultandoRegistrosTest extends EntityManagerTest {
 	public void atualizarReferencia () {
 		Produto produto = entityManager.find(Produto.class, 1);
 		produto.setNome("Microfone");
-		
-		//depois de alterar o produto, eu do um refresh para ele pegar novamente o estado do banco de dados.		
 		entityManager.refresh(produto);
-		
 		Assert.assertEquals("Kindle", produto.getNome());
 	}
+	
 	
 	 
 	
---
+++ src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
@@ -0,0 +1,22 @@
+package com.algaworks.ecommerce.iniciandocomjpa;
+
+import org.junit.Test;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Produto;
+
+public class OperacoesComTransacaoTest extends EntityManagerTest{
+		
+	@Test
+	public void abrirEFecharTransacao() {
+		Produto produto = new Produto();
+		entityManager.getTransaction().begin();
+		
+//		entityManager.persist(produto);
+//		entityManager.merge(produto);
+//		entityManager.remove(produto);
+		
+		entityManager.getTransaction().commit();
+	}
+	
+}
