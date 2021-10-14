abc24d696a2918bcfa3eba6e65ce02f8473a9312
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2021-10-14 09:54:38 -0400

    feat: test para buscar dados no banco por meio do identificador.

--- src/test/java/com/algaworks/ecommerce/iniciandocomjpa/ConsultandoRegistrosTest.java
+++ src/test/java/com/algaworks/ecommerce/iniciandocomjpa/ConsultandoRegistrosTest.java
@@ -6,14 +6,20 @@ import javax.persistence.Persistence;
 
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.algaworks.ecommerce.model.Produto;
 
 public class ConsultandoRegistrosTest {
 	
 	private static EntityManagerFactory entityManagerFactory;
 	
 	private EntityManager entityManager;
+	
+	//METODOS DE CALLBACK	
 	
 	//anotação do metodo de callback do junit.	
 	@BeforeClass
@@ -38,6 +44,35 @@ public class ConsultandoRegistrosTest {
 	public void tearDown() {
 		entityManager.close();
 	}
+	
+	
+	//COMEÇO DOS TESTS.
+	
+	@Test
+	public void buscarPorIdentificador() {
+		Produto produto = entityManager.find(Produto.class, 1);
+		
+		//get reference vai fazer uma busca no banco só se for utilizado algum atributo de Produto, como por exemplo produto.getNome().		
+//		Produto produto = entityManager.getReference(Produto.class, 1);
+
+		//o retorno não pode ser null		
+		Assert.assertNotNull(produto);
+		
+		//o retorno de produto.nome tem que ser Kindle		
+		Assert.assertEquals("Kindle", produto.getNome()); 
+	}
+	
+	@Test
+	public void atualizarReferencia () {
+		Produto produto = entityManager.find(Produto.class, 1);
+		produto.setNome("Microfone");
+		
+		//depois de alterar o produto, eu do um refresh para ele pegar novamente o estado do banco de dados.		
+		entityManager.refresh(produto);
+		
+		Assert.assertEquals("Kindle", produto.getNome());
+	}
+	
 	
 	
 }
